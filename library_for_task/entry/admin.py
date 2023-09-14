dbf4e8156384411545a14b02c05e2ee03af589d8
Vlad <129696560+Vladlength@users.noreply.github.com>
2023-09-14 17:11:42 +0300

    first_com
    first_com

--- library_for_task/entry/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Lib_User, Book
-# register the purchase model to see it in the admin panel
-
-admin.site.register(Lib_User)
-admin.site.register(Book)
